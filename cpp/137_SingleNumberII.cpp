23b486f3442cf04d7032bd5a34a7152c97158749
umiringo <umiringo42@gmail.com>
2015-12-15 22:05:58 +0800

    add 137

--- cpp/137_SingleNumberII.cpp
+++ cpp/137_SingleNumberII.cpp
@@ -21,3 +21,14 @@ public:
 };
 
 //with 2 variable
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        for(auto num : nums){
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        return ones | twos;
+    }
+};
