bb7196205d4bb0067754b51de9a9760c33708413
umiringo <umiringo42@gmail.com>
2015-12-18 23:19:46 +0800

    modify 014 add clever solution

--- cpp/014_LongestCommonPrefix.cpp
+++ cpp/014_LongestCommonPrefix.cpp
@@ -15,3 +15,22 @@ public:
         return cPrefix;
     }
 };
+
+//clear
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+        if(strs.empty()) return prefix;
+       
+        int index = 0;
+        while(index < strs[0].size()){
+            for(int i = 1; i < strs.size(); ++i){
+                if(index > strs[i].size() || strs[i][index] != strs[0][index] ) return prefix;
+            }
+            prefix += strs[0][index];
+            index++;
+        }
+        return prefix;
+    }
+};
